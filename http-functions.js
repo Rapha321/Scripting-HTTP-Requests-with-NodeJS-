2e63657a88c0b1e6df55319e61365cdd05935b96
Rapha321 <ralpha19@hotmail.com>
2018-12-03 19:20:05 +0000

    Step 5 - Make it a Module

---
+++ http-functions.js
@@ -0,0 +1,33 @@
+
+
+module.exports = function getHTML (options, callback) {
+
+    // while https is built-in to Node, it is a module, so it must be required
+  var https = require('https');
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    let buffer = "";
+    response.on('data', function (data) {
+      buffer += data + '\n';
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      callback(buffer);
+      // console.log('Chunk Received. Length:', buffer);
+      // console.log('Response stream complete.');
+    });
+  });
+};
+
+
+
+
